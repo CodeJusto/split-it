d1ad12d02f99ebb0f083501ff383f2a262f3b24c
Patrick Peng <me@patrickpeng.ca>
2016-06-12 12:18:58 -0700

    setup Amazon API

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -12,7 +12,7 @@ $(document).on('page:load', function() {
   });
 
 
-  $('.inviteLink').attr("href", (window.location.hostname + ':3000/invite/' + $('.inviteLink').data("key")));
+  //$('.inviteLink').attr("href", (window.location.hostname + ':3000/invite/' + $('.inviteLink').data("key")));
   // console.log("Test")
 
 
