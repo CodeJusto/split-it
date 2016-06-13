9f060bd3eab40d3eceb68d0347ff7db19df9600b
Patrick Peng <me@patrickpeng.ca>
2016-06-13 16:30:23 -0700

    update cart total + fix amazon api

--- app/controllers/carts/products_controller.rb
+++ app/controllers/carts/products_controller.rb
@@ -6,12 +6,24 @@ class Carts::ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
 
-    @product.external_id = /\/(dp|gp\/product)\/(.+)\//.match(@product.url)[2].to_s
 
-    if @product.save
+    if !get_amazon_product(@product).nil?
+      amazon = get_amazon_product(@product)
+      @product.image = amazon['ImageSets']['ImageSet'][0]['LargeImage']['URL']
+      @product.price = amazon["OfferSummary"]["LowestNewPrice"]["Amount"]
+      @product.description = amazon["ItemAttributes"]["Feature"].join(";")
+      @product.price_checked_at = DateTime.now
+      if @product.save
       redirect_to cart_path(params[:cart_id]), notice: "#{@product.display_name} was added successfully!"
+      else
+        render :new
+      end
     else
-      render :new
+      if @product.save
+        redirect_to cart_path(params[:cart_id]), notice: "#{@product.display_name} was added successfully!"
+      else
+        render :new_full
+      end
     end
   end
 
@@ -38,8 +50,20 @@ class Carts::ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(
-      :display_name, :url, :quantity, :cart_id
+      :display_name, :url, :quantity, :cart_id, :price, :price_checked_at, :description, :image
     )
+  end
+
+  def get_amazon_product(product)
+    product.external_id = /\/(dp|gp\/product)\/(.+)\//.match(product.url)[2].to_s
+
+    response = $amazon_request.item_lookup(
+      query: {
+        'ItemId' => product.external_id,
+        'ResponseGroup' => 'ItemAttributes,Small,Images,OfferSummary'
+      }
+    )
+    response.to_h["ItemLookupResponse"]["Items"]["Item"]
   end
 
 end
