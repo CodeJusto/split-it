3a9af82b5e550fc134044216d0f90ddc416f8d84
Patrick Peng <me@patrickpeng.ca>
2016-06-13 09:46:14 -0700

    fix cart total issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,18 @@
 class Product < ActiveRecord::Base
   belongs_to :cart
+
+  validates :display_name, presence: true
+  validates :url, presence: true
+  validates :cart_id, presence: true
+  validates :quantity, presence: true
+  # validate :amazon_url, on: :create
+
+  private
+
+  def amazon_url
+    external_id = /\/(dp|gp\/product)\/(.+)\//.match(url)[2].to_s
+    if external_id.nil?
+      errors.add(:url, "is not a valid Amazon URL")
+    end
+  end
 end
