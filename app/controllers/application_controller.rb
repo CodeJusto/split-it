1d62d3cd714da4485091184f4cd08d67c8b8d2c4
Patrick Peng <me@patrickpeng.ca>
2016-06-14 09:35:31 -0700

    add format_price helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,18 @@ class ApplicationController < ActionController::Base
  
   protect_from_forgery with: :exception
 
+  def format_price(price)
+    dollar = price.to_i > 0 ? price / 100.00 : 0
+    "CDN$ #{dollar}"
+  end
+
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
-  helper_method :current_user
-
+  helper_method :current_user, :format_price
 end
+
+
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,13 +11,17 @@ class Cart < ActiveRecord::Base
 
   before_save :convert_minimum_payment_to_cents
 
+  # helper ApplicationHelper
+
   def expiry_date_must_be_in_the_future 
     errors.add(:expiry, "must be in the future") if !expiry.blank? and expiry < Date.today
   end
 
   def total
-    total = products.sum(:price) > 0 ? products.sum(:price) / 100.00 : 0
-    "CDN$ #{total}"
+    # total = products.sum(:price) > 0 ? products.sum(:price) / 100.00 : 0
+    # "CDN$ #{total}"
+    # format_price(products.sum(:price))
+    products.sum(:price)
   end
 
   private
