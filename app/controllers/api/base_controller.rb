e4c0c8bf1a92c8a7f738e5020be77ea9ac78b043
ceebs <cthiessen@gmail.com>
2016-06-23 12:27:42 -0700

    retrying authenticity skip

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,8 +1,7 @@
 class Api::BaseController < ApplicationController
 
-  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
-
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+skip_before_filter :verify_authenticity_token
+protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
   
   def current_user
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
  
-  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
-
+  skip_before_filter :verify_authenticity_token
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
   protected
