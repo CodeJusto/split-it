49c209e40b6abb25ad97b29453ea4418fb484b7a
Patrick Peng <me@patrickpeng.ca>
2016-06-22 23:31:03 -0700

    update login page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,5 +33,13 @@ window.fbAsyncInit = function() {
  }(document, 'script', 'facebook-jssdk'));
 
 $(document).ready(function(){
-  $('.modal-trigger').leanModal();
+
+  $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      ready: function() { $('body').css("overflow-y", "scroll"); }
+    }
+  );
 });
