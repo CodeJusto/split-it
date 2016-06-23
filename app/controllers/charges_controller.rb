133fde34627aa9c29afbdd58972311cc292a0e69
ceebs <cthiessen@gmail.com>
2016-06-23 11:32:06 -0700

    fixing charge controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,118 +1,118 @@
-class ChargesController < ApplicationController
-  include CartsHelper
+# class ChargesController < ApplicationController
+#   include CartsHelper
 
-  skip_before_filter :verify_authenticity_token
+#   skip_before_filter :verify_authenticity_token
 
-  def new
+#   def new
     
-  end
+#   end
 
-  def create
+#   def create
     
-    @amount = convert_to_cents(params[:amount])
-    @cart_id = params[:cart]
-    @cart = Cart.where(id: @cart_id)
-    token = params[:stripeToken]
+#     @amount = convert_to_cents(params[:amount])
+#     @cart_id = params[:cart]
+#     @cart = Cart.where(id: @cart_id)
+#     token = params[:stripeToken]
 
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
+#     customer = Stripe::Customer.create(
+#       :email => params[:stripeEmail],
+#       :source  => params[:stripeToken]
+#     )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'    
-    )
+#     charge = Stripe::Charge.create(
+#       :customer    => customer.id,
+#       :amount      => @amount,
+#       :description => 'Rails Stripe customer',
+#       :currency    => 'usd'    
+#     )
     
-    @payment = Payment.new(
-      user_id: current_user.id,
-      cart_id: @cart_id,
-      stripe_customer_id: charge.customer,
-      stripe_charge_id: charge.id,
-      amount: charge.amount
-    ) 
+#     @payment = Payment.new(
+#       user_id: current_user.id,
+#       cart_id: @cart_id,
+#       stripe_customer_id: charge.customer,
+#       stripe_charge_id: charge.id,
+#       amount: charge.amount
+#     ) 
     
-    @payee = User.where(id: @payment.user_id)
+#     @payee = User.where(id: @payment.user_id)
 
-    @payment.save
-    if @payment.save
-      @cart = Cart.find(@payment.cart_id)
-      @updated_cart_total = calculate_total_payments(get_cart_payments(@cart.id))
-      @goal = @cart.total
-      @updated_pctg = cart_progress(@updated_cart_total, @goal)
-      # @total = @cart.payments.sum(:amount)
+#     @payment.save
+#     if @payment.save
+#       @cart = Cart.find(@payment.cart_id)
+#       @updated_cart_total = calculate_total_payments(get_cart_payments(@cart.id))
+#       @goal = @cart.total
+#       @updated_pctg = cart_progress(@updated_cart_total, @goal)
+#       # @total = @cart.payments.sum(:amount)
 
-      organizer_email = find_role(1, 'email')
-      contributor_email = find_role(2, 'email')
-      Notifications.update_contributor(organizer_email, @payee, @payment).deliver_now unless organizer_email.empty?
+#       organizer_email = find_role(1, 'email')
+#       contributor_email = find_role(2, 'email')
+#       Notifications.update_contributor(organizer_email, @payee, @payment).deliver_now unless organizer_email.empty?
       
-      unless contributor_email.empty?
-        contributor_email.each do |contributor|
-          Notifications.send_invoice(contributor, @payment, @cart).deliver_now
-        end
-      end
+#       unless contributor_email.empty?
+#         contributor_email.each do |contributor|
+#           Notifications.send_invoice(contributor, @payment, @cart).deliver_now
+#         end
+#       end
 
-      organizer_text = find_role(1, 'text')
-      contributor_text = find_role(2, 'text')
+#       organizer_text = find_role(1, 'text')
+#       contributor_text = find_role(2, 'text')
 
       
-      unless organizer_text.empty?
-        organizer_text.each do |text| 
-          $twilio.account.sms.messages.create(
-            :from => ENV['COMPANY_PHONE'],
-            :to => "+1#{text.number}",
-            :body => "#{@payee[0].name} has contributed #{@payment.amount} for a cart!"
-          )
-        end
-      end
+#       unless organizer_text.empty?
+#         organizer_text.each do |text| 
+#           $twilio.account.sms.messages.create(
+#             :from => ENV['COMPANY_PHONE'],
+#             :to => "+1#{text.number}",
+#             :body => "#{@payee[0].name} has contributed #{@payment.amount} for a cart!"
+#           )
+#         end
+#       end
 
-      unless contributor_text.empty?
-        contributor_text.each do |text| 
-          $twilio.account.sms.messages.create(
-            :from => ENV['COMPANY_PHONE'],
-            :to => "+1#{text.number}",
-            :body => "You have has contributed #{@payment.amount} for a cart!"
-          )
-        end
-      end
+#       unless contributor_text.empty?
+#         contributor_text.each do |text| 
+#           $twilio.account.sms.messages.create(
+#             :from => ENV['COMPANY_PHONE'],
+#             :to => "+1#{text.number}",
+#             :body => "You have has contributed #{@payment.amount} for a cart!"
+#           )
+#         end
+#       end
 
-      Notification.create(cart_id: @cart_id, notification_template_id: 2)
+#       Notification.create(cart_id: @cart_id, notification_template_id: 2)
 
-      respond_to do |t|
-        t.json do
-          render :json => {
-            :payment => format_price(@payment.amount),
-            :payee => @payee,
-            :updated_cart_total => format_price(@updated_cart_total),
-            :updated_pctg => @updated_pctg,
-            :stripe_charge_id => charge.id
-            }
-        end
-        t.html do
-          redirect(back)
-        end
-      if request.xhr?
-      render :json => {
-        :payment => format_price(@payment.amount),
-        :payee => @payee,
-        :updated_cart_total => format_price(@updated_cart_total),
-        :updated_pctg => @updated_pctg,
-        :stripe_charge_id => charge.id
-        }                   
-      else
-        redirect(back)
-      end
-    else
-      redirect(back)
-    end
+#       respond_to do |t|
+#         t.json do
+#           render :json => {
+#             :payment => format_price(@payment.amount),
+#             :payee => @payee,
+#             :updated_cart_total => format_price(@updated_cart_total),
+#             :updated_pctg => @updated_pctg,
+#             :stripe_charge_id => charge.id
+#             }
+#         end
+#         t.html do
+#           redirect(back)
+#         end
+#       if request.xhr?
+#       render :json => {
+#         :payment => format_price(@payment.amount),
+#         :payee => @payee,
+#         :updated_cart_total => format_price(@updated_cart_total),
+#         :updated_pctg => @updated_pctg,
+#         :stripe_charge_id => charge.id
+#         }                   
+#       else
+#         redirect(back)
+#       end
+#     else
+#       redirect(back)
+#     end
 
 
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_charge_path
+#     rescue Stripe::CardError => e
+#       flash[:error] = e.message
+#       redirect_to new_charge_path
 
-  end
+#   end
 
-end
+# end
