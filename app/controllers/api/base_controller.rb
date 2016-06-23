0710fb6dd2c09089058425259fe78989e27441ca
ceebs <cthiessen@gmail.com>
2016-06-23 12:23:09 -0700

    skipping auth for demo in base controller

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,6 +1,9 @@
 class Api::BaseController < ApplicationController
 
-  skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
   
   def current_user
     @current_user ||= User.find(params[:id]) unless params[:id].nil?
