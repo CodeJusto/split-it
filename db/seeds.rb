a0c4fe46786e2ab6b0df5375650f13fdbfe38b08
Patrick Peng <me@patrickpeng.ca>
2016-06-24 12:37:21 -0700

    update seed file for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ User.create(name: "Monica Olinescu", email: "MonicaO@ceebs.faith", password: "pa
 User.create(name: "Khurram Virani", email: "KhurramV@ceebs.faith", password: "password", image: 'https://s3.amazonaws.com/lighthouse-compass/uploads/teacher/custom_avatar/1/thumb_khurram-virani-2.jpg')
 User.create(name: "James Sapara", email: "JamesS@ceebs.faith", password: "password", image: "https://avatars.githubusercontent.com/u/118081?v=3")
 User.create(name: "Jeremy Holman", email: "JeremyH@ceebs.faith", password: "password", image: "https://avatars.githubusercontent.com/u/172523?v=3")
-
 User.create(name: "Justin", email: "justincwong89@gmail.com", password: 'password', number: "7782286061", image: "https://scontent.fsnc1-2.fna.fbcdn.net/v/t1.0-1/p160x160/11002626_10205136913775914_5838250906441272135_n.jpg?oh=30f4e6a97a518bc88179a0e454d5262b&oe=58053257")
+User.create(name: "Chad", email:"chad@chad.com", password: "password", number: "7788888888", image: "https://pbs.twimg.com/profile_images/451489167659524096/5eHq-FXQ.jpeg")
 
 Cart.create(status_id: 1, expiry: Time.new(2016, 8, 31), name: "New couch for the office", country: "Canada", street_address: "19-8591 Blundell Road", city: "Richmond", province: "British Columbia", zip_code: "V6Y 1K2")
 
